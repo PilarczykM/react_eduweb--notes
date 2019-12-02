ffe51436df70ec2bb690c7649d424fa8b2752d91
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-12-02 21:50:20 +0100

    Adding global styles and new font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap');
+    font-family: 'Montserrat', sans-serif;
+    &,
+    &::after,
+    &::before{
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+  }
+`;
+
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <h1>Boiler plate.</h1>
-    </div>
+    </>
   );
 }
 
