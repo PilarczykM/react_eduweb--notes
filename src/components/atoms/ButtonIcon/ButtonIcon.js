2744536be2bf0cdbac10da285be2013783cdfc4d
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-12-03 09:20:37 +0100

    Adding button icon to components.

--- src/components/atoms/ButtonIcon/ButtonIcon.js
+++ src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -1,5 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const ButtonIcon = styled.button``;
+const ButtonIcon = styled.button`
+  width: 67px;
+  height: 6.7rem;
+  border: none;
+  border-radius: 2rem;
+  background-image: url(${({ icon }) => icon});
+  background-repeat: no-repeat;
+  background-position: 50%, 50%;
+  background-size: 40%;
+  background-color: ${({ active }) => (active ? '#fff' : 'transparent')};
+`;
 
 export default ButtonIcon;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -5,6 +5,10 @@ import Button from '../../components/atoms/Button/Button';
 import Heading from '../../components/atoms/Heading/Heading';
 import Input from '../../components/atoms/Input/Input';
 import Paragraph from '../../components/atoms/Paragraph/Paragraph';
+import ButtonIcon from '../../components/atoms/ButtonIcon/ButtonIcon';
+
+import loopIcon from '../../assets/icons/bulb.svg';
+import searchIcon from '../../assets/icons/pen.svg';
 
 const Root = () => (
   // Theme provider pass props.theme to all childs.
@@ -19,6 +23,9 @@ const Root = () => (
     <br />
     <Input placeholder="Email" />
     <Input placeholder="Search" search />
+    <br />
+    <ButtonIcon icon={loopIcon} />
+    <ButtonIcon icon={searchIcon} active />
   </ThemeProvider>
 );
 
