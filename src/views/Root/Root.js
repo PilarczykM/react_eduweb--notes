82d466c8792081124a3716116e745eae2633c283
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-12-03 12:26:15 +0100

    Adding router to project

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Heading from '../../components/atoms/Heading/Heading';
 import MainTemplate from '../../template/MainTemplate';
+import Notes from '../Notes';
+import Articles from '../Articles';
+import Twitters from '../Twitters';
 
 const Root = () => (
   // Theme provider pass props.theme to all childs.
   <MainTemplate>
-    <Heading>Test</Heading>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Notes} />
+        <Route path="/articles" exact component={Articles} />
+        <Route path="/twitters" exact component={Twitters} />
+      </Switch>
+    </Router>
   </MainTemplate>
 );
 
