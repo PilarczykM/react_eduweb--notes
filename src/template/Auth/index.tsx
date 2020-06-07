61131f891449bd5363b418eb57dcfd4f682193fb
Marcin Pilarczyk <marcinpilarczyk@me.com>
2020-06-07 17:23:19 +0200

    Add messages if something goes wrong.
    Add redirect if user is logged in firebase.

--- src/template/Auth/index.tsx
+++ src/template/Auth/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Heading } from '../../components/atoms/Heading';
 import * as S from './Auth.styled';
+import { Redirect } from 'react-router';
+import { routes } from '../../routes';
+import { useSelector } from 'react-redux';
+import { State } from '../../store/types';
 
 
 const logoImg = require("../../assets/icons/logo.svg")
 
 
 
-export const AuthTemplate: React.FC = ({ children }) => (
+export const AuthTemplate: React.FC = ({ children }) => {
+  const user = useSelector((state: State) => state.user);
+  
+  return user.email ? <Redirect to={routes.home} /> :(
   <S.Wrapper>
     <S.Logo src={logoImg} alt="" />
     <Heading>Your new favorite online notes experience</Heading>
     <S.AuthCard>{children}</S.AuthCard>
   </S.Wrapper>
-);
+)};
 
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,24 +1,19 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Button } from '../../components/atoms/Button';
 import { routes } from '../../routes';
-import { State } from '../../store/types';
 import { AuthTemplate } from '../../template/Auth';
 import * as S from './LoginPage.styled';
 import { inputValuesProps } from './types';
 import { validateEmail, isEmpty } from '../../services/utils';
 import { login } from '../../store/user/userThunk';
-import { Redirect } from 'react-router';
 
 export const LoginPage: React.FC = () => {
-  // TODO: Podpiac redux thunk jak tylko backend zadziala.
-
   const [inputValues, setInputValues] = useState<inputValuesProps>({
     email: '',
     password: '',
   });
 
-  const user = useSelector((state: State) => state.user);
   const dispatch = useDispatch();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,13 +26,14 @@ export const LoginPage: React.FC = () => {
   };
 
   const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const {email, password} = inputValues;
+    // Todo: Dodac jakis ładny message, informujacy uzytkownika o problemach podczas logowania.
+    const { email, password } = inputValues;
     if (!validateEmail(email)) { console.log("Email is not valid!"); return };
     if (isEmpty(email) || isEmpty(password)) { console.log("Values can not be empty!"); return };
-    dispatch(login({email, password}));
+    dispatch(login({ email, password }));
   }
 
-  return user.email ? <Redirect to={routes.home} /> : (
+  return (
     <AuthTemplate>
       <S.StyledHeading>Sign in</S.StyledHeading>
       <S.Form>
